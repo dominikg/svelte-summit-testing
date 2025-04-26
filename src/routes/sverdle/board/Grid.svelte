6cb177f3af22a206551da74b46f55536f47e142f
Jeppe Reinhold <jeppe@reinhold.is>
2025-04-26 21:09:15 +0200

    Extract Sverdle into components (#2)
    * add packageManager for corepack
    
    * extract sverdle Cell to separate component
    
    * extract sverdle Row into separate component
    
    * extract Grid into separate component, as well as the whole Keyboard
    
    * format all files
    
    * update pnpm to latest
    
    * rename Won.svelte -> Result.svelte

---
+++ src/routes/sverdle/board/Grid.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Row from './Row.svelte';
+
+	interface Props {
+		guesses: string[];
+		answers: string[];
+		currentIndex: number;
+		currentGuess: string;
+		isPlaying: boolean;
+		isBadGuess: boolean;
+	}
+
+	let { guesses, answers, currentIndex, currentGuess, isPlaying, isBadGuess }: Props = $props();
+</script>
+
+<div class="grid" class:playing={isPlaying} class:bad-guess={isBadGuess}>
+	{#each Array.from(Array(6).keys()) as row (row)}
+		<Row
+			rowNumber={row}
+			isCurrentRow={row === currentIndex}
+			{isBadGuess}
+			{isPlaying}
+			guess={guesses[row] ?? ''}
+			answer={answers[row] ?? ''}
+			{currentGuess}
+		/>
+	{/each}
+</div>
+
+<style>
+	.grid {
+		--width: min(100vw, 40vh, 380px);
+		max-width: var(--width);
+		align-self: center;
+		justify-self: center;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+	}
+</style>
---
+++ src/routes/sverdle/controls/Letter.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	interface Props {
+		letter: string;
+		classname: 'exact' | 'close' | 'missing' | undefined;
+		desc: string | undefined;
+		submittable: boolean;
+		onUpdate: (event: MouseEvent) => void;
+	}
+
+	let { letter, classname, desc, submittable, onUpdate }: Props = $props();
+</script>
+
+<button
+	onclick={onUpdate}
+	data-key={letter}
+	class={classname}
+	disabled={submittable}
+	formaction="?/update"
+	name="key"
+	value={letter}
+	aria-label="{letter} {desc || ''}"
+>
+	{letter}
+</button>
+
+<style>
+	button {
+		--size: min(8vw, 4vh, 40px);
+		background-color: white;
+		color: black;
+		width: var(--size);
+		border: none;
+		border-radius: 2px;
+		font-size: calc(var(--size) * 0.5);
+		margin: 0;
+	}
+
+	button.exact {
+		background: var(--color-theme-2);
+		color: white;
+	}
+
+	button.missing {
+		opacity: 0.5;
+	}
+
+	button.close {
+		border: 2px solid var(--color-theme-2);
+	}
+
+	button:focus {
+		background: var(--color-theme-1);
+		color: white;
+		outline: none;
+	}
+</style>
