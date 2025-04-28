d207b70d2c3d2c1bad634d27557677121f55b75c
dominikg <dominik.goepel@gmx.de>
2025-04-28 20:57:53 +0200

    add divide function

--- src/lib/math.spec.ts
+++ src/lib/math.spec.ts
@@ -1,7 +1,19 @@
 import { describe, it, expect } from 'vitest';
-import { sum } from './math.ts'
+import { sum, divide } from './math.ts';
+
 describe('sum', () => {
-	it('adds 1 and 2 correctly', () => {
-		expect(sum(1,2)).toBe(3);
+	it('adds 1 and 2', () => {
+		expect(sum(1, 2)).toBe(3);
+	});
+	it('works with negative numbers', () => {
+		expect(sum(-1, -1)).toBe(-2);
+	});
+});
+describe('divide', () => {
+	it('divides 1 by 2', () => {
+		expect(divide(1, 2)).toBe(0.5);
+	});
+	it('throws for division by 0', () => {
+		expect(() => divide(1, 0)).toThrow();
 	});
 });
--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,3 +1,10 @@
 export function sum(a: number, b: number): number {
 	return a + b;
 }
+
+export function divide(a: number, b: number): number {
+	if (b === 0) {
+		throw new Error('division by 0');
+	}
+	return a / b;
+}
