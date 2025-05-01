08306c4393bbbd2e0fe58b398afc9517e6fa7bf5
dominikg <dominik.goepel@gmx.de>
2025-05-01 16:33:23 +0200

    add component test for Result.svelte

---
+++ src/routes/sverdle/controls/Result.svelte.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest';
+import {render} from '@testing-library/svelte';
+import Result from './Result.svelte'
+describe('Result.svelte',()=>{
+	it('celebrates winning',()=>{
+		const {container, getByRole} = render(Result,{props:{won: true}});
+		const button = getByRole('button');
+		expect(button).toHaveTextContent('you won :)');
+		expect(container.querySelector('.confetti-container')).toBeInTheDocument();
+	})
+	it('shows answer when losing',()=>{
+		const {container,getByRole} = render(Result,{props:{won: false, answer: '42'}});
+		const button = getByRole('button');
+		expect(button).toHaveTextContent('game over :(');
+		expect(container).toHaveTextContent('the answer was "42"');
+	})
+})
