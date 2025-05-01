c5e3dadf7175f7146ca3df8e9ffb91e78a550f43
dominikg <dominik.goepel@gmx.de>
2025-05-01 20:34:23 +0200

    add example of reactive component test

---
+++ src/lib/Greeter.svelte.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+import { render } from '@testing-library/svelte';
+import { flushSync } from 'svelte';
+import Greeter from './Greeter.svelte';
+describe('Greeter.svelte', () => {
+	it('shows name', () => {
+		const { container } = render(Greeter, { props: { name: 'Svelte Summit' } });
+		expect(container).toHaveTextContent('Svelte Summit');
+	});
+	it('can be tested with reactive state', () => {
+		const props = $state({ name: 'Svelte Summit' });
+		const { container } = render(Greeter, { props });
+		expect(container).toHaveTextContent('Svelte Summit');
+		props.name = 'Barcelona';
+		flushSync();
+		expect(container).not.toHaveTextContent('Svelte Summit');
+		expect(container).toHaveTextContent('Barcelona');
+	});
+});
