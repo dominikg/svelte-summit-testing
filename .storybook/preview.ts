63398f771bfd0e91eae77e715e8efff5863c6cb5
Jeppe Reinhold <jeppe@reinhold.is>
2025-05-02 13:49:30 +0200

    sort stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -16,6 +16,59 @@ const preview: Preview = {
 			// 'error' - fail CI on a11y violations
 			// 'off' - skip a11y checks entirely
 			test: 'todo'
+		},
+		options: {
+			storySort: (a, b) => {
+				if (a.id === b.id) {
+					return 0;
+				}
+
+				if (a.title.includes('Board/Cell')) {
+					return -1;
+				}
+				if (b.title.includes('Board/Cell')) {
+					return 1;
+				}
+				if (a.title.includes('Board/Row')) {
+					return -1;
+				}
+				if (b.title.includes('Board/Row')) {
+					return 1;
+				}
+				if (a.title.includes('Board/Grid')) {
+					return -1;
+				}
+				if (b.title.includes('Board/Grid')) {
+					return 1;
+				}
+
+				if (a.title.includes('Controls/Letter')) {
+					return -1;
+				}
+				if (b.title.includes('Controls/Letter')) {
+					return 1;
+				}
+				if (a.title.includes('Controls/Row')) {
+					return -1;
+				}
+				if (b.title.includes('Controls/Row')) {
+					return 1;
+				}
+				if (a.title.includes('Controls/Keyboard')) {
+					return -1;
+				}
+				if (b.title.includes('Controls/Keyboard')) {
+					return 1;
+				}
+				if (a.title.includes('Controls/Result')) {
+					return -1;
+				}
+				if (b.title.includes('Controls/Result')) {
+					return 1;
+				}
+
+				return a.title.localeCompare(b.title, undefined, { numeric: true });
+			}
 		}
 	}
 };
