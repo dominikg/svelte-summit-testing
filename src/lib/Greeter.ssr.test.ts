0a37c188526c1db4bd4f708e79371a506b64545c
dominikg <dominik.goepel@gmx.de>
2025-05-02 21:44:15 +0200

    add greeter ssr test

---
+++ src/lib/Greeter.ssr.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import {render } from 'svelte/server';
+import Greeter from './Greeter.svelte';
+describe('Greeter.svelte SSR', () => {
+	it('renders name',()=>{
+		const {body} = render(Greeter,{props:{name:'foo'}});
+		expect(body).toContain('foo')
+	})
+});
