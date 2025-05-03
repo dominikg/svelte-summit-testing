dca8c781205ed3a5d52f2325a98ba64c21582ef8
dominikg <dominik.goepel@gmx.de>
2025-05-03 19:20:29 +0200

    refactor: use adapter-node

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run build && PORT=4173 node build/index.js',
 		port: 4173,
 	},
 	testDir: 'e2e'
